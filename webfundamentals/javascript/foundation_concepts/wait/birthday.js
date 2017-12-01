ea7f38dce01ebb6971eec14bcbd93e6d9755851d
Amaury <azapien@hotmail.com>
2017-12-01 16:35:19 -0600

    added algorithm to program number of days till bday

---
+++ webfundamentals/javascript/foundation_concepts/wait/birthday.js
@@ -0,0 +1,9 @@
+var daysUntilMyBirthday = 79;
+
+if (daysUntilMyBirthday > 60) {
+  console.log(daysUntilMyBirthday + "days until my birthday. Sad sad days...");
+} else if(daysUntilMyBirthday <= 30 > 1) {
+  consol.log(daysUntilMyBirthday, "lets gooooo!");
+} else{
+  console.log("Happy Birthday!!!!");
+}
