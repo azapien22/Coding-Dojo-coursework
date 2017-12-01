383aa42fc7d32341040f5bac5fddd90ad8842e27
Amaury <azapien@hotmail.com>
2017-12-01 10:28:01 -0600

    added script in html and js file

---
+++ webfundamentals/javascript/foundation_concepts/declare/ideclare.js
@@ -0,0 +1,26 @@
+let num1 = 55;
+let num2 = 105;
+let num3 = 22;
+let num4 = 23;
+
+let string1 = "hello";
+let string2 = "what is declaring?";
+let string3 = "Declaring things are cool.";
+
+let dontDeclare = false;
+let doDeclare = true;
+
+let myVar;
+
+console.log("The first numb: ", num1);
+console.log("the second numb:", num2);
+console.log("the third numb:", num3);
+console.log("the fourth numb:", num4);
+
+console.log(string1, "world");
+console.log(string2 + ":" + string3);
+
+console.log("Dont declare is :", dontDeclare);
+console.log("Do declare is :", doDeclare);
+
+console.log("my undefined variable :", myVar);
