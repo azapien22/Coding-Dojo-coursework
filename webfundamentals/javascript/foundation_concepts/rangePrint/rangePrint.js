1810ecfe2119c6c11f44cbb10d3d2af82b202b81
Amaury <azapien@hotmail.com>
2017-12-03 20:28:39 -0600

    added a function to express rangePrint

---
+++ webfundamentals/javascript/foundation_concepts/rangePrint/rangePrint.js
@@ -0,0 +1,23 @@
+function printRange(start, end, iterate){
+    for (var i = start; i < end; i += iterate) {
+        console.log(i);
+    }
+}
+printRange(2, 10, 2);
+
+// Bonus
+
+function printRangeBonus(start, end, iterate){
+    if(!end){
+        end = start;
+        start = 0;
+    }
+    if(!iterate){
+        iterate = 1;
+    }
+    for (var i = start; i < end; i += iterate) {
+        console.log(i);
+    }
+}
+printRange(4, 8);
+printRange(4);
