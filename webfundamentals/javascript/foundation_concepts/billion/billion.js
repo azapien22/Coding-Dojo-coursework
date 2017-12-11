594c603d4211549057c03e7d87667d3ccf1cdd50
Amaury <azapien@hotmail.com>
2017-12-11 13:45:09 -0600

    modified the function

--- webfundamentals/javascript/foundation_concepts/billion/billion.js
+++ webfundamentals/javascript/foundation_concepts/billion/billion.js
@@ -1,3 +1,4 @@
+// sample
 var earnings = .01;
 var amount = .01;
 for (var i = 2; i <= 100000; i++) {
@@ -8,3 +9,22 @@ for (var i = 2; i <= 100000; i++) {
     if(amount == Infinity){
         console.log(i);
         break;
+
+// Actual code
+var sum = 0.01;
+var daysUntilTenThousand= 1;
+var daysUntilBillion = 1;
+
+for(var i =1; i<31; i++){
+    sum = sum + sum;
+    if(sum < 10000){
+    daysUntilTenThousand++;
+    }
+    if (sum <1000000000){
+      daysUntilBillion++
+    }
+  }
+
+console.log(sum);
+console.log(daysUntilTenThousand);
+console.log(daysUntilBillion);
