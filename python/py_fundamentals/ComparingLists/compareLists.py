1aace889cf63bc75ebf9e0fec567bed3efca8070
Amaury <azapien@hotmail.com>
2018-01-10 21:58:18 -0600

    compare lists py practice

---
+++ python/py_fundamentals/ComparingLists/compareLists.py
@@ -0,0 +1,15 @@
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','milk']
+
+if list_one == list_two:
+    print "The lists are the same"
+else:
+    print "The lists are not the same"
+
+list_one = [32,65,98,17,55,64]
+list_two = [38,52,14,65,92,55]
+
+if list_one == list_two:
+    print "The lists are the same"
+else:
+    print "The lists are not the same"
